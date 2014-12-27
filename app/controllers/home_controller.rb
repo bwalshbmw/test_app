303aa1c60ef522279c9273d80028bc8de0e761a0
Brian Walsh <bmw@useed.org>
2014-12-26 19:59:54 -0500

    Added a form for the Home demo

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,2 +1,19 @@
 class HomeController < ApplicationController
+  
+  def index
+    result = params[:answer]
+    
+    if result
+      @message = "The answer is #{result}. Would you like to add another?"
+    else
+      @message = "Hello world! I can add numbers if you'd like..."
+    end
+  end
+  
+  def form_submit
+    result = params[:a].to_i + params[:b].to_i + params[:c].to_i
+    
+    redirect_to "/?answer=#{result}"
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  
+  get '/', to: 'home#index'
+  post '/submit', to: 'home#form_submit'
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
